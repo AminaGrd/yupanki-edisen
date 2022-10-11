1005e29e88c4234a5b8316568b1ca11f81dc3960
AminaGrd <amina.giraud@gmail.com>
2022-10-11 16:18:17 +0200

    color accessibilities

--- content-homepage.php
+++ content-homepage.php
@@ -12,7 +12,7 @@ $featured_image = get_the_post_thumbnail_url( get_the_ID(), 'thumbnail' );
 
 <!-- <div id="post-<?php the_ID(); ?>" <?php post_class(); ?> style="<?php storefront_homepage_content_styles(); ?>" data-featured-image="<?php echo esc_url( $featured_image ); ?>"> -->
 	<div class="col-full">
-		<!-- <?php do_action( 'storefront_homepage' ); ?> -->
+		<!-- <?php do_action( 'storefront_homepage' ); ?> 
 		<!-- Modifications childstorefront -->
 
 		<!-- Fin modifications -->
