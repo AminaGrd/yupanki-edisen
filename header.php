8f4b33370e43630e62a3f18ab59daa2ffab74250
AminaGrd <amina.giraud@gmail.com>
2022-11-08 11:54:06 +0100

    metadescription header

--- header.php
+++ header.php
@@ -12,6 +12,7 @@
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="Édisen est une maison d’édition à compte d'éditeur à Dijon et Bourg-en-Bresse.">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
